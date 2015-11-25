806f763d040e3f4e052ce92075def820abeba66c
Christopher Sasarak <cmsonl@rit.edu>
2015-11-25 18:44:25 -0500

    Added functions for calculating node distance and a NodeRating type

--- src/Node.hs
+++ src/Node.hs
@@ -7,12 +7,27 @@ import Data.ByteString.Base16 (encode)
 import Control.Applicative
 import System.Random
 import Data.Word
+import Data.Bits
 
 data Node = Node { nodeId :: BS.ByteString }
+
+data RatedNode = Good Node
+               | Bad Node
+               | Questionable Node
 
 -- Generates a node with a new ByteString given a random number generator
 genNode :: RandomGen g => g -> Node
 genNode gen = Node id
               where id = BS.pack . take 20 . randoms $ gen 
 
+-- Base hash compare function, 
+compareHashes :: BS.ByteString -> BS.ByteString -> BS.ByteString
+compareHashes bs1 bs2 = BS.pack $ BS.zipWith xor bs1 bs2
 
+-- Distance between two nodes
+nodeDistance :: Node -> Node -> BS.ByteString
+nodeDistance n1 n2 = compareHashes (nodeId n1) (nodeId n2)
+
+-- Distance between a node and an arbitrary hash
+nodeHashCmp :: Node -> BS.ByteString -> BS.ByteString
+nodeHashCmp n h = compareHashes (nodeId n) h
